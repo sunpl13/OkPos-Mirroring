75328a9cc80ab233097ff88df279e4fb0f6bf4ab
Seungjin Lee <dltmdwls154@gmail.com>
2022-10-22 03:44:58 +0900

    User Detail Modal 추가

--- src/components/list/ListTemplate.js
+++ src/components/list/ListTemplate.js
@@ -1,55 +1,57 @@
-import React, {useCallback, useEffect, useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import {CSmartTable} from '../custom/smart-table/CSmartTable'
-import {CSmartPagination} from '../custom/pagination/CSmartPagination'
-import {CCardBody} from '@coreui/react'
 import PropTypes from 'prop-types'
+import {CBadge} from '@coreui/react'
 
-const ListTemplate = ({items, size, handleRowClick}) => {
+const ListTemplate = ({items, onClick}) => {
   const [listItems, setListItems] = useState([])
-  const [itemTotalCount] = useState(0)
-  const [currentPage, setCurrentPage] = useState(1)
-  const downloadUsers = useCallback(() => {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(result => {
-        setListItems(result)
-      })
-  }, [])
+  const getBadge = status => {
+    switch (status) {
+      case true:
+        return 'success'
+      case false:
+        return 'danger'
+      default:
+        return 'primary'
+    }
+  }
 
   useEffect(() => {
-    downloadUsers()
-  }, [downloadUsers])
-  const test = item => {
-    console.log(item)
-  }
+    setListItems(items)
+  }, [items])
+
   return (
-    <CCardBody>
-      <CSmartTable
-        items={listItems}
-        activePage={currentPage}
-        columnFilter
-        columnSorter
-        pagination
-        clickableRows
-        onRowClick={test}
-        tableProps={{
-          hover: true,
-          responsive: true,
-        }}
-        itemsPerPageSelect
-        itemsPerPage={10}
-      />
-    </CCardBody>
+    <CSmartTable
+      items={listItems}
+      activePage={1}
+      columnFilter
+      columnSorter
+      pagination
+      clickableRows
+      onRowClick={onClick}
+      tableProps={{
+        hover: true,
+        responsive: true,
+        striped: true,
+        align: 'middle',
+      }}
+      scopedColumns={{
+        status: ({status}) => (
+          <td>
+            <CBadge color={getBadge(status)}>{status ? 'Activate' : 'Disabled'}</CBadge>
+          </td>
+        ),
+      }}
+      noItemsLabel={'Not Date'}
+      itemsPerPageSelect
+      itemsPerPage={10}
+    />
   )
 }
 
 ListTemplate.propTypes = {
   items: PropTypes.array,
-  size: PropTypes.number,
-  handleRowClick: PropTypes.func,
-  page: PropTypes.number,
-  handleSortList: PropTypes.func,
-  handlePageChange: PropTypes.func,
+  onClick: PropTypes.func,
 }
 
 export default ListTemplate
--- src/pages/users/userlist.js
+++ src/pages/users/userlist.js
@@ -1,4 +1,4 @@
-import React, {useRef, useState} from 'react'
+import React, {useState} from 'react'
 import {
   CCard,
   CCardBody,
@@ -6,64 +6,28 @@ import {
   CCol,
   CForm,
   CButton,
-  CNav,
-  CNavItem,
-  CNavLink,
   CRow,
-  CTabContent,
-  CTabPane,
+  CModalBody,
+  CModal,
+  CModalHeader,
+  CModalTitle,
+  CFormLabel,
+  CFormInput,
 } from '@coreui/react'
-import {testOptions, testTableColumns, testTableValues} from '../test/testConstant'
-import TestInfo from '../../components/TestInfo'
-import TestInfo1 from '../../components/TestInfo1'
-import {CSmartTable} from '../../components/custom/smart-table/CSmartTable'
-import {CSmartPagination} from '../../components/custom/pagination/CSmartPagination'
-import SortSelect from '../../components/forms/selectForm/SortSelect'
-import SearchInput from '../../components/forms/inputForm/SearchInput'
+import {testUserTableValues} from '../test/testConstant'
 import ListTemplate from '../../components/list/ListTemplate'
 
 const UserList = () => {
-  const [page, setPage] = useState(1)
-  const size = 10
-
-  const [setTestName] = useState('')
-
-  const [selectedOption, setSelectedOption] = useState({
-    value: 'test1',
-    label: '테스트1',
-  })
-
   const [items, setItems] = useState([])
-  const [itemTotalCount] = useState(0)
-
-  const [activeKey, setActiveKey] = useState('')
-  const [selectedTestId, setSelectedTestId] = useState(0)
-  const testInfoRef = useRef()
-
-  const handleSearchOnKeyPress = event => {
-    if (event.key === 'Enter') {
-      console.log(event.key)
-    }
-  }
-  const handleSortList = event => {
-    setSelectedOption(event)
-  }
-  function handlePageChange(selectedPage) {
-    if (selectedPage === 0) {
-      return
-    }
-    setPage(selectedPage)
-    handleRetrieveTestList(selectedPage).then()
-  }
-
-  function handleRowClick(item) {
-    setSelectedTestId(item.id)
-    setActiveKey('TestInfo')
-    //testInfoRef.current.handleRetrieveTest(item.id)
+  const [selectedItem, setSelectedItem] = useState({})
+  const [showModal, setShowModal] = useState(false)
+  const handleRowClick = item => {
+    setSelectedItem(item)
+    setShowModal(!showModal)
   }
 
   const handleRetrieveTestList = async () => {
-    setItems(testTableValues)
+    setItems(testUserTableValues)
   }
 
   return (
@@ -72,94 +36,49 @@ const UserList = () => {
         <CCard className='mb-4'>
           <CCardHeader>
             <CForm className='row g-3'>
-              <SortSelect
-                md={4}
-                sm={6}
-                label={'정렬'}
-                options={testOptions}
-                value={selectedOption}
-                onchange={handleSortList}
-              />
-              <SearchInput
-                md={4}
-                label='UserSearch'
-                id='userSearchInput'
-                placeholder='UserSearch'
-                onKeyPress={handleSearchOnKeyPress}
-                onChange={e => setTestName(e.target.value)}
-              />
               <CCol xs={12}>
-                <CButton color='primary' onClick={() => handleRetrieveTestList(page)}>
+                <CButton color='primary' onClick={handleRetrieveTestList}>
                   조회하기
                 </CButton>
               </CCol>
             </CForm>
           </CCardHeader>
           <CCardBody>
-            <ListTemplate
-              columns={testTableColumns}
-              items={items}
-              itemsPerPage={size}
-              clickableRows
-              onRowClick={handleRowClick}
-              page={page}
-              onActivePageChange={handlePageChange}
-            />
+            <ListTemplate items={items} onClick={handleRowClick} />
           </CCardBody>
         </CCard>
       </CCol>
-      <hr />
-      <CCol xs={12}>
-        <CCard className='mb-4'>
-          <CCardHeader>
-            <strong>상세 정보를 보기 위해 너드를 선택해주세요.</strong>
-          </CCardHeader>
-          <CCardBody>
-            <CNav>
-              <CNavItem>
-                <CNavLink
-                  href='#'
-                  active={activeKey === 'TestInfo'}
-                  disabled={selectedTestId === 0}
-                  onClick={e => {
-                    e.preventDefault()
-                    setActiveKey('TestInfo')
-                  }}
-                >
-                  테스트 정보
-                </CNavLink>
-              </CNavItem>
-              <CNavItem>
-                <CNavLink
-                  href='#'
-                  active={activeKey === 'TestInfo1'}
-                  disabled={selectedTestId === 0}
-                  onClick={e => {
-                    e.preventDefault()
-                    setActiveKey('TestInfo1')
-                  }}
-                >
-                  테스트 정보 1
-                </CNavLink>
-              </CNavItem>
-            </CNav>
-            <CTabContent>
-              <CTabPane className='p-3' visible={activeKey === 'TestInfo'}>
-                <TestInfo ref={testInfoRef} />
-                <hr />
-              </CTabPane>
-              <CTabPane className='p-3' visible={activeKey === 'TestInfo1'}>
-                {activeKey === 'TestInfo1' ? (
-                  <TestInfo1 selectedTestId={selectedTestId} sendExample={activeKey} />
-                ) : (
-                  <></>
-                )}
-                <hr />
-              </CTabPane>
-            </CTabContent>
-          </CCardBody>
-        </CCard>
-      </CCol>
+      <CModal size='lg' visible={showModal} onClose={() => setShowModal(false)}>
+        <CModalHeader>
+          <CModalTitle>{`User ID : ${selectedItem.id}`}</CModalTitle>
+        </CModalHeader>
+        <CModalBody>
+          <CRow className='mb-3'>
+            <CFormLabel htmlFor='staticEmail' className='col-sm-2 col-form-label'>
+              id
+            </CFormLabel>
+            <CCol sm={10}>
+              <CFormInput type='text' id='staticEmail' defaultValue={selectedItem.id} readOnly />
+            </CCol>
+          </CRow>
+          <CRow className='mb-3'>
+            <CFormLabel htmlFor='staticEmail' className='col-sm-2 col-form-label'>
+              Email
+            </CFormLabel>
+            <CCol sm={10}>
+              <CFormInput type='text' id='staticEmail' defaultValue={selectedItem.email} readOnly />
+            </CCol>
+          </CRow>
+          <CRow className='mb-3'>
+            <CFormLabel htmlFor='staticEmail' className='col-sm-2 col-form-label'>
+              Phone Number
+            </CFormLabel>
+            <CCol sm={10}>
+              <CFormInput type='text' id='staticEmail' defaultValue={selectedItem.phoneNumber} readOnly />
+            </CCol>
+          </CRow>
+        </CModalBody>
+      </CModal>
     </CRow>
   )
 }
