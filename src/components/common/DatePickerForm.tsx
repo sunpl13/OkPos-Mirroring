df15b2258f07be640569de96d926cd6e6119b917
jungsoo <sunpl13@naver.com>
2022-10-26 14:56:18 +0900

    [#2] feat : date-picker 컴포넌트 생성

---
+++ src/components/common/DatePickerForm.tsx
@@ -0,0 +1,62 @@
+import {useEffect, useState} from 'react'
+import {CCol, CFormLabel} from '@coreui/react'
+import DatePicker from 'react-datepicker'
+import styled from 'styled-components'
+import 'react-datepicker/dist/react-datepicker.css'
+
+// CSS Modules, react-datepicker-cssmodules.css
+// import 'react-datepicker/dist/react-datepicker-cssmodules.css';
+
+interface IDatePickerProps {
+  id: string
+  date: Date
+  setDate: (date: Date) => void
+  label: string
+  readOnly?: boolean
+}
+
+const DatePickerForm = ({id, date, setDate, label, readOnly}: IDatePickerProps) => {
+  const [startDate, setStartDate] = useState(new Date())
+
+  useEffect(() => {
+    setStartDate(date)
+  }, [date])
+
+  const onChange = (date: Date) => {
+    setDate(date)
+    setStartDate(date)
+  }
+  return (
+    <>
+      <CFormLabel htmlFor='staticEmail' className='col-sm-2 col-form-label'>
+        {label || ' * '}
+      </CFormLabel>
+      <CCol>
+        <CDatePicker
+          readOnly={readOnly}
+          dateFormat='yyyy-MM-dd'
+          selected={startDate}
+          onChange={date => onChange(date)}
+        />
+      </CCol>
+    </>
+  )
+}
+
+export default DatePickerForm
+
+const CDatePicker = styled(DatePicker)`
+  display: block;
+  width: 100%;
+  padding: 0.375rem 0.75rem;
+  font-size: 1rem;
+  font-weight: 400;
+  line-height: 1.5;
+  color: var(--cui-input-color, rgba(44, 56, 74, 0.95));
+  background-color: var(--cui-input-bg, #fff);
+  background-clip: padding-box;
+  border: 1px solid var(--cui-input-border-color, #b1b7c1);
+  appearance: none;
+  border-radius: 0.375rem;
+  transition: border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+`
