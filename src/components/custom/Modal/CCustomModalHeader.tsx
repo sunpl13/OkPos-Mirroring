9cf1005321f19841eaf4583a5c2d2402c084ef3a
jungsoo <sunpl13@naver.com>
2022-11-01 15:00:29 +0900

    [#2] feat : CustomModal Header 컴포넌트 생성
    모달 x 버튼 클릭 시 추가 로직필요할 때 사용

---
+++ src/components/custom/Modal/CCustomModalHeader.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+interface IProps {
+  children:JSX.Element | string
+  onClick: ()=>void
+}
+
+const CCustomModalHeader = ({children,onClick}: IProps) => {
+  return (
+    <div className='modal-header'>
+    <h5 className='modal-title'>{children}</h5>
+    <button type='button' className='btn btn-close' onClick={onClick} aria-label='Close'></button>
+  </div>
+  )
+}
+
+export default CCustomModalHeader
