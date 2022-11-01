25f0ebbd0585c70e5eae664db0b56266edd1b4d1
jungsoo <sunpl13@naver.com>
2022-11-01 15:01:49 +0900

    [#2] feat : 모달 종료 확인 알럿 모달 생성
    ex) edit 활성화 상태에서 모달 종료하려 할때 사용

---
+++ src/components/Modal/CloseCheckModal.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import {CButton, CHeader, CModal, CModalBody, CModalFooter, CModalHeader, CModalTitle} from '@coreui/react'
+
+interface IProps {
+  onClick: () => void
+  visible: boolean
+  setVisible: (state: boolean) => void
+}
+
+const CloseCheckModal = ({onClick, visible, setVisible}: IProps) => {
+  return (
+    <CModal visible={visible} alignment='center'>
+      <CHeader>
+        <CModalTitle>정말 페이지에서 나가시겠습니까?</CModalTitle>
+        <CModalBody>지금 페이지를 나가시면 변경사항이 저장되지 않습니다.</CModalBody>
+        <CModalFooter>
+          <CButton color='danger' onClick={onClick}>
+            나가기
+          </CButton>
+          <CButton color='primary' onClick={() => setVisible(false)}>
+            Cancel
+          </CButton>
+        </CModalFooter>
+      </CHeader>
+    </CModal>
+  )
+}
+
+export default CloseCheckModal
