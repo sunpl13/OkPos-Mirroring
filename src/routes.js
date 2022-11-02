d8a8b65a0a288add6193191a252858e0a1c99bcd
김지환 <dev.jihwan.gmail.com>
2022-11-02 23:22:23 +0900

    routes 페이지별 코드 구분, 주석 생성

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,22 @@ const Dashboard = React.lazy(() => import('./pages/dashboard/Dashboard'))
 const TestList = React.lazy(() => import('./pages/test/TestList'))
 const CreateTest = React.lazy(() => import('./pages/test/CreateTest'))
 
+// ------ 오케이포스 홈페이지
+
+const Employment = React.lazy(() => import('./pages/employment/Employment'))
+const Popup = React.lazy(() => import('./pages/popup/PopUpPage'))
+
+// ------ 가맹점 전용 공식몰 페이지
+
+// 회원관리
+const OfficailMallUserList = React.lazy(() => import('./pages/officialMall/users/Userlist'))
+const OfficailMallWithdrawalUserList = React.lazy(() => import('./pages/officialMall/users/WithdrawalUsers'))
+
+// 1:1 문의 관리
+const OfficailMallInquiryList = React.lazy(() => import('./pages/officialMall/inquiry/InquiryList'))
+
+// ------ 대리점 전용 파트너 센터
+
 // 유저 리스트
 const UserList = React.lazy(() => import('./pages/partnerCenter/users/Userlist'))
 const WithdrawalUsers = React.lazy(() => import('./pages/partnerCenter/users/WithdrawalUsers'))
@@ -21,23 +37,30 @@ const DealerSupportList = React.lazy(() => import('./pages/partnerCenter/agency/
 // 자료실
 const MaterialList = React.lazy(() => import('./pages/partnerCenter/material/MaterialList'))
 
-const Employment = React.lazy(() => import('./pages/employment/Employment'))
-const Popup = React.lazy(() => import('./pages/popup/PopUpPage'))
-
-// 가맹점 전용 공식몰 페이지
-
-// 회원관리
-const OfficailMallUserList = React.lazy(() => import('./pages/officialMall/users/Userlist'))
-const OfficailMallWithdrawalUserList = React.lazy(() => import('./pages/officialMall/users/WithdrawalUsers'))
-
-// 1:1 문의 관리
-const OfficailMallInquiryList = React.lazy(() => import('./pages/officialMall/inquiry/InquiryList'))
-
 const routes = [
   {path: '/dashboard', name: 'Dashboard', element: Dashboard},
 
   {path: `/test/test-list`, name: '조회', element: TestList},
   {path: `/test/create-test`, name: '생성', element: CreateTest},
+
+  // ------ 오케이포스 홈페이지
+
+  //employment
+  {path: `/home/employment`, name: '채용관리', element: Employment},
+
+  //popup
+  {path: `/home/popup`, name: '팝업관리', element: Popup},
+
+  // ------ 가맹점 전용 공식몰 페이지
+
+  // 회원관리
+  {path: `/official/users`, name: '회원관리', element: OfficailMallUserList},
+  {path: `/official/withdrawalUsers`, name: '탈퇴회원관리', element: OfficailMallWithdrawalUserList},
+
+  // 1:1 문의 관리
+  {path: `/official/inquirys`, name: '문의관리', element: OfficailMallInquiryList},
+
+  // ------ 대리점 전용 파트너 센터
 
   // user
   {path: `/users/userList`, name: 'UserList', element: UserList},
@@ -45,28 +68,15 @@ const routes = [
 
   // materialList
   {path: `/material/materialList`, name: 'MaterialList', element: MaterialList},
-  //employment
-  {path: `/home/employment`, name: '채용관리', element: Employment},
-
-  //popup
-  {path: `/home/popup`, name: '팝업관리', element: Popup},
-
-  //UserInquiry
-  {path: `/users/inquiry/userInquiryList`, name: 'UserInquiryList', element: UserInquiryList},
-
-  // 가맹점 전용 공식몰 페이지
-
-  // 회원관리
-  {path: `/official/users`, name: '회원관리', element: OfficailMallUserList},
-  {path: `/official/withdrawalUsers`, name: '탈퇴회원관리', element: OfficailMallWithdrawalUserList},
-
-  {path: `/official/inquirys`, name: '문의관리', element: OfficailMallInquiryList},
 
   //Notice
   {path: `/users/notice/noticeList`, name: 'NoticeList', element: NoticeList},
 
   // Dealer Support
   {path: '/users/dealerSupport', name: 'DealerSupport', element: DealerSupportList},
+
+  //UserInquiry
+  {path: `/users/inquiry/userInquiryList`, name: 'UserInquiryList', element: UserInquiryList},
 ]
 
 export default routes
