e87302fd0d9c0b1f2ae2297ebc157700e881b447
jungsoo <sunpl13@naver.com>
2022-11-12 01:30:41 +0900

    [#23] feat : 공지사항 리스트 컬럼 명 설정

---
+++ src/utils/columns/homePage/notice/Columns.js
@@ -0,0 +1,17 @@
+export const homePageNoticeColumns = [
+  {
+    key: 'notictId',
+    label: 'No',
+    _props: {color: 'primary', className: 'notictId'},
+  },
+  {
+    key: 'title',
+    label: '공고제목',
+    _props: {color: 'primary', className: 'title'},
+  },
+  {
+    key: 'startedAt',
+    label: '등록일',
+    _props: {color: 'primary', className: 'startedAt'},
+  },
+]
