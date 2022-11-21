0891309a39c937e8a7bae94c0e365a3eff85d26d
jungsoo <sunpl13@naver.com>
2022-11-21 23:50:04 +0900

    [#25] feat : 권한관리 UI

---
+++ src/pages/member/MemberMagement.js
@@ -0,0 +1,97 @@
+import {useEffect, useState} from 'react'
+import PageHeader from '../../components/common/PageHeader'
+import {CCard, CCardBody, CCardHeader, CCol, CForm, CButton, CRow} from '@coreui/react'
+import ListTemplate from '../../components/list/ListTemplate'
+import {EditorColumns} from '../../utils/columns/member/Columns'
+import ApiConfig, {HttpMethod} from '../../dataManager/apiConfig'
+import {EndPoint} from '../../dataManager/apiMapper'
+const MemberManageMent = () => {
+  const [items, setItems] = useState([])
+  const [showModal, setShowModal] = useState(false)
+  const [isReadOnly, setIsReadOnly] = useState(true)
+  const [selectedItem, setSelectedItem] = useState({
+    number: -1,
+    editorId: 4,
+    loginId: '',
+    createdAt: '',
+    accessAuthority: '',
+    isAuthorized: 1,
+  })
+
+  const onLoadMemberManageMentList = async () => {
+    try {
+      const data = await ApiConfig.request({
+        data: {},
+        query: {},
+        path: {},
+        method: HttpMethod.GET,
+        url: EndPoint.EDITOR,
+      })
+      setItems(data?.data.result.responses)
+    } catch (error) {
+      alert(error)
+    }
+  }
+
+  //가져오기 onLoad
+  //생성 onCreate
+  //수정 onUpdate
+  //삭제 onDelete
+
+  useEffect(() => {
+    onLoadMemberManageMentList()
+  }, [])
+
+  const handleShowMemberManageMentDetailModal = async item => {
+    setShowModal(!showModal)
+  }
+
+  const handleMemberManageMentAddModal = () => {
+    setIsReadOnly(false)
+    setSelectedItem({
+      number: -1,
+      editorId: 1,
+      loginId: '',
+      createdAt: '',
+      accessAuthority: '',
+      isAuthorized: 1,
+    })
+    setShowModal(!showModal)
+  }
+
+  return (
+    <>
+      <PageHeader title='회원관리' />
+      <CRow>
+        <CCol xs={12}>
+          <CCard className='mb-4'>
+            <CCardHeader>
+              <CForm className='row g-3'>
+                <CCol xs={1}>
+                  <CButton color='primary' onClick={onLoadMemberManageMentList}>
+                    조회하기
+                  </CButton>
+                </CCol>
+                <CCol xs={1}>
+                  <CButton color='primary' onClick={handleMemberManageMentAddModal}>
+                    추가
+                  </CButton>
+                </CCol>
+              </CForm>
+            </CCardHeader>
+            <CCardBody>
+              <ListTemplate
+                items={items}
+                onClick={handleShowMemberManageMentDetailModal}
+                columns={EditorColumns}
+                className={'userList'}
+              />
+            </CCardBody>
+          </CCard>
+        </CCol>
+      </CRow>
+    </>
+  )
+}
+
+export default MemberManageMent
