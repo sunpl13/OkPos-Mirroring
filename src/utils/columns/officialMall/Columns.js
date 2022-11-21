b9afe8f51dc6226b6963b7a4f7e71a03fb456a18
김지환 <dev.jihwan.gmail.com>
2022-11-22 00:42:10 +0900

    refactor : custom css 변경

--- src/utils/columns/officialMall/Columns.js
+++ src/utils/columns/officialMall/Columns.js
@@ -1,32 +1,32 @@
 // 회원 리스트
 export const userListColumns = [
-  {key: 'userId', label: 'No', sorter: false, filter: false},
-  {key: 'name', label: '이름'},
-  {key: 'email', label: '이메일'},
-  {key: 'phoneNumber', label: '휴대전화번호'},
-  {key: 'createdAt', label: '최초등록일', sorter: false, filter: false},
-  {key: 'status', label: '상태', sorter: false, filter: false},
+  {key: 'userId', label: 'No', sorter: false, filter: false, _props: {className: 'no'}},
+  {key: 'name', label: '이름', _props: {className: 'name'}},
+  {key: 'email', label: '이메일', _props: {className: 'email'}},
+  {key: 'phoneNumber', label: '휴대전화번호', _props: {className: 'phoneNumber'}},
+  {key: 'createdAt', label: '최초등록일', sorter: false, filter: false, _props: {className: 'createdAt'}},
+  {key: 'status', label: '상태', sorter: false, filter: false, _props: {className: 'status'}},
 ]
 
 // 탈퇴회원 리스트
 export const resignUserListColumns = [
-  {key: 'userId', label: 'No', sorter: false, filter: false},
-  {key: 'name', label: '이름'},
-  {key: 'email', label: '이메일'},
-  {key: 'phoneNumber', label: '휴대전화번호'},
-  {key: 'resignReason', label: '탈퇴사유'},
-  {key: 'createdAt', label: '최초등록일', sorter: false, filter: false},
-  {key: 'updatedAt', label: '계정탈퇴일', sorter: false, filter: false},
-  {key: 'status', label: '상태', sorter: false, filter: false},
+  {key: 'userId', label: 'No', sorter: false, filter: false, _props: {className: 'no'}},
+  {key: 'name', label: '이름', _props: {className: 'name'}},
+  {key: 'email', label: '이메일', _props: {className: 'email'}},
+  {key: 'phoneNumber', label: '휴대전화번호', _props: {className: 'phoneNumber'}},
+  {key: 'resignReason', label: '탈퇴사유', _props: {className: 'resignReason'}},
+  {key: 'createdAt', label: '최초등록일', sorter: false, filter: false, _props: {className: 'createdAt'}},
+  {key: 'updatedAt', label: '계정탈퇴일', sorter: false, filter: false, _props: {className: 'updatedAt'}},
+  {key: 'status', label: '상태', sorter: false, filter: false, _props: {className: 'status'}},
 ]
 
 // 1:1 문의 리스트
 export const inquiryListColumns = [
-  {key: 'inquiryId', label: 'No', sorter: false, filter: false},
-  {key: 'category', label: '문의 유형'},
-  {key: 'name', label: '이름'},
-  {key: 'email', label: '이메일'},
-  {key: 'phoneNumber', label: '휴대전화번호'},
+  {key: 'inquiryId', label: 'No', sorter: false, filter: false, _props: {className: 'no'}},
+  {key: 'category', label: '문의 유형', _props: {className: 'category'}},
+  {key: 'name', label: '이름', _props: {className: 'name'}},
+  {key: 'email', label: '이메일', _props: {className: 'email'}},
+  {key: 'phoneNumber', label: '휴대전화번호', _props: {className: 'phoneNumber'}},
 ]
 
 // FAQ 리스트
