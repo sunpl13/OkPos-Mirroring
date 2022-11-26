2de77363a81e8fe8232de9fec47c69026e14b6a2
jungsoo <sunpl13@naver.com>
2022-11-26 23:41:09 +0900

    [#2] feat : 파일다운로드(뷰어만) 컴포넌트 생성

---
+++ src/components/forms/inputForm/ModalFilesView.js
@@ -0,0 +1,59 @@
+import {PaperClipOutlined} from '@ant-design/icons'
+import styled from 'styled-components'
+import {CCol} from '@coreui/react'
+import {useEffect, useState} from 'react'
+import {antdImageFormat} from '../../../utils/awsCustom'
+
+const ModalFilesView = ({fileItem}) => {
+  const [fileList, setFileList] = useState([])
+  useEffect(() => {
+    if (fileItem && fileItem.length > 0) {
+      setFileList(
+        fileItem.map(path => {
+          const nameArr = path.split('/')
+          return {
+            uid: path,
+            name: nameArr[nameArr.length - 1],
+            status: 'done',
+            url: antdImageFormat(path),
+          }
+        }),
+      )
+    }
+  }, [fileItem])
+
+  return (
+    <CCol>
+      {fileList.map(item => (
+        <DivBox key={item.uid}>
+          <PaperClipOutlined />
+          <a>{item.name}</a>
+        </DivBox>
+      ))}
+    </CCol>
+  )
+}
+
+export default ModalFilesView
+
+const DivBox = styled.div`
+  display: flex;
+  align-items: center;
+  :hover {
+    background-color: rgba(0, 0, 0, 0.04);
+  }
+  & a {
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    padding: 0 8px;
+    line-height: 1.5714285714285714;
+    flex: auto;
+    transition: all 0.3s;
+    color: #1677ff !important;
+    text-decoration: none;
+    background-color: transparent;
+    outline: none;
+    cursor: pointer;
+  }
+`
