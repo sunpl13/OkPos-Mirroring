6209d4b85e590a86646eaf9af143130ae671479a
jungsoo <sunpl13@naver.com>
2022-12-03 22:46:17 +0900

    [#35] feat : data room 리스트 컬럼 설정

---
+++ src/utils/columns/EnHomePage/dataRoom/Columns.js
@@ -0,0 +1,17 @@
+export const dataRoomColumns = [
+  {
+    key: 'dataRoomEnglishId',
+    label: 'No',
+    _props: {color: 'primary', className: 'dataRoomEnglishId'},
+  },
+  {
+    key: 'title',
+    label: '제목',
+    _props: {color: 'primary', className: 'title'},
+  },
+  {
+    key: 'categoryKorean',
+    label: '카테고리',
+    _props: {color: 'primary', className: 'categoryKorean'},
+  },
+]
