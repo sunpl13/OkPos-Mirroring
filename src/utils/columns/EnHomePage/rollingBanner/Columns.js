e8edff89f232391fe82085ccd2bda9bd4b5da277
jungsoo <sunpl13@naver.com>
2022-12-06 17:56:40 +0900

    [#30] feat: 영문 홈페이지 롤링배너 리스트 컬럼 설정

---
+++ src/utils/columns/EnHomePage/rollingBanner/Columns.js
@@ -0,0 +1,17 @@
+export const rollingBannerColumns = [
+  {
+    key: 'bannerEnglishId',
+    label: 'No',
+    _props: {color: 'primary', className: 'bannerEnglishId'},
+  },
+  {
+    key: 'title',
+    label: '배너 타이틀',
+    _props: {color: 'primary', className: 'title'},
+  },
+  {
+    key: 'image',
+    label: '배너 이미지',
+    _props: {color: 'primary', className: 'image'},
+  },
+]
