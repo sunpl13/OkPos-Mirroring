d62fff50474fa7671b40adcd149bacdad81944c9
jungsoo <sunpl13@naver.com>
2022-12-31 01:41:09 +0900

    [#25] bugfix : 다른 도메인에서 안보이는 현상 수정

--- src/utils/columns/member/Columns.js
+++ src/utils/columns/member/Columns.js
@@ -3,11 +3,14 @@ export const EditorColumns = [
     key: 'isAuthorized',
     label: '',
     _props: {color: 'primary', className: 'isAuthorized'},
+    filter: false,
+    sorter: false,
   },
   {
     key: 'number',
     label: 'No',
     _props: {color: 'primary', className: 'number'},
+    filter: false,
   },
   {
     key: 'loginId',
@@ -18,10 +21,12 @@ export const EditorColumns = [
     key: 'createdAt',
     label: '최초 등록일',
     _props: {color: 'primary', className: 'createdAt'},
+    filter: false,
   },
   {
     key: 'editAuthorityKorean',
     label: '권한',
     _props: {color: 'primary', className: 'editAuthorityKorean'},
+    filter: false,
   },
 ]
