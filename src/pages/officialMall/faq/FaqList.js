6b6cfb171718bcfd68f44d61dc5273dcc3848dd0
김지환 <dev.jihwan.gmail.com>
2023-01-18 02:00:31 +0900

    [#12, #16, #17]refactor:  에디터 벨리데이션 수정

--- src/pages/officialMall/faq/FaqList.js
+++ src/pages/officialMall/faq/FaqList.js
@@ -221,7 +221,7 @@ const FaqList = () => {
     if (!category || !_.includes(categoryVals, category)) return alert('카테고리를 선택해주세요')
     if (!title) return alert('제목을 입력해주세요.')
     if (title.length > 50) return alert('제목 글자수 초과입니다.(50자)')
-    if (!content) return alert('본문을 입력해주세요.')
+    if (content.replace(/<[^>]*>?|/g, '').length === 0) return alert('본문을 입력해주세요.')
     if (content.replace(/<[^>]*>?|/g, '').length > 300) return alert('본문 글자수 초과입니다.')
 
     if (window.confirm('저장 하시겠습니까?')) {
