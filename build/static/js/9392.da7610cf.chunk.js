3fc617f25317428bf42fca6769f9cbf3b261d6fc
Yubin <busygirljanehwang@gmail.com>
2023-12-15 20:13:28 +0900

    release/2023.12.15

---
+++ build/static/js/9392.da7610cf.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkneordinary_template_react_js_admin=self.webpackChunkneordinary_template_react_js_admin||[]).push([[9392],{39392:(e,a,r)=>{r.r(a),r.d(a,{default:()=>n});r(72791);var s=r(80184);const n=()=>(0,s.jsxs)(s.Fragment,{children:[(0,s.jsx)("br",{}),(0,s.jsx)("br",{})]})}}]);
+//# sourceMappingURL=9392.da7610cf.chunk.js.map
